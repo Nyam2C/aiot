4d102bef0786711992a9ea5b37e22dffdb0ed4fd
T_hyeon <qkrxogus113@ajou.ac.kr>
2025-06-10 03:19:11 +0900

    [FEAT] server

---
+++ server.js
@@ -0,0 +1,30 @@
+const Cancellation = require('./src/server/Cancellation');
+const Info = require('./src/server/Info');
+const Reservation = require('./src/server/Reservation');
+
+
+var solace = require('solclientjs').debug; 
+
+var factoryProps = new solace.SolclientFactoryProperties();
+factoryProps.profile = solace.SolclientFactoryProfiles.version10;
+solace.SolclientFactory.init(factoryProps);
+
+solace.SolclientFactory.setLogLevel(solace.LogLevel.WARN);
+
+var cancellation = new Cancellation(solace, 'CBUM','Ticket/reservation>');
+cancellation.run(process.argv);
+
+var info = new Info(solace, 'CBUM','Ticket/info>');
+info.run(process.argv);
+
+var reservation = new Reservation(solace, 'CBUM','Ticket/cancellation>');
+reservation.run(process.argv);
+
+
+process.stdin.resume();
+
+process.on('SIGINT', function () {
+    'use strict';
+    process.exit();
+});
+
